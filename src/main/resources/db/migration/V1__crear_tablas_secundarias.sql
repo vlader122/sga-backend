8efd555acd826117ef18dbb9a118520e5e1ac663
niver.copa <vlader122@gmail.com>
2025-01-30 19:09:48 -0400

    eliminar migraciones

--- src/main/resources/db/migration/V1__crear_tablas_secundarias.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public.personas (nombre, apellido, email, telefono) VALUES
-('AAAAA', 'Pérez', 'juan.perez@example.com', '555-1234'),
-('BBBBB', 'González', 'maria.gonzalez@example.com', '555-5678'),
-('CCCCC', 'Ramírez', 'carlos.ramirez@example.com', '555-8765'),
-('DDDDD', 'Martínez', 'laura.martinez@example.com', '555-4321');
