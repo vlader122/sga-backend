f51983d0aa8da038767d0a25466f9e90129c71f1
Niver Copa <47153990+vlader122@users.noreply.github.com>
2025-01-30 19:40:55 -0400

    Update SecurityConfiguration.java

--- src/main/java/com/blumbit/vlader122/sga/config/SecurityConfiguration.java
+++ src/main/java/com/blumbit/vlader122/sga/config/SecurityConfiguration.java
@@ -45,7 +45,6 @@ public class SecurityConfiguration {
             .authorizeHttpRequests(request -> request
                 .requestMatchers(SWAGGER_WHITELIST).permitAll()
                 .requestMatchers("/api/v1/auth/**").permitAll()
-                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/api/v1/**").authenticated()
                 .anyRequest().authenticated())
             .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
